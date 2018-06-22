edc5d7732a349a82165be05b3211e783a1a99607
Tyler Gubala <gubalatyler@yahoo.com>
2018-06-22 18:49:33 -0400

    push for pip

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(name='bpy',
       author_email='gubalatyler@gmail.com',
       license='GPL-3.0',
       setup_requires=[
-          'bpybuild'
+          'bpybuild@1.0.0a0'
       ],                   
       cmdclass={
           'install': CustomInstallHandler
