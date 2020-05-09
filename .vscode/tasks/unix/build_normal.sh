1f653f8d9e38d0b011672eab18b94c5debe28e0a
Tyler Gubala <gubalatyler@yahoo.com>
2020-05-09 15:37:19 -0400

    2.79  python3.5  configuration

--- .vscode/tasks/unix/build_normal.sh
+++ .vscode/tasks/unix/build_normal.sh
@@ -3,8 +3,8 @@ if [ -f setup.py ]; then
 fi
 cp "bpy/setup.py" .
 rm -Rf build
-./venv/3.7-32/bin/python3 setup.py bdist_wheel > logs/3.7-32_build.log 2>&1
-./venv/3.7-64/bin/python3 setup.py sdist bdist_wheel > logs/3.7-64_build.log 2>&1
+./venv/3.5-32/bin/python3 setup.py bdist_wheel > logs/3.5-32_build.log 2>&1
+./venv/3.5-64/bin/python3 setup.py sdist bdist_wheel > logs/3.5-64_build.log 2>&1
 if [ -f setup.py ]; then
     rm setup.py
 fi
--- bpy/setup.py
+++ bpy/setup.py
@@ -37,7 +37,7 @@ SYSTEM_OS_NAME = platform.system()
 # Change the Blender desired API version variable to build different versions
 # of the Blender API. For instance, 'v2.79b' is the same version of the API
 # as you would get when opening the Blender application at v2.79b
-VERSION = "2.82"
+VERSION = "2.79"
 VERSION_TUPLE = pkg_resources.parse_version(VERSION)
 
 class CMakeExtension(Extension):
