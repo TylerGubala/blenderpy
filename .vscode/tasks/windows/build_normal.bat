198a11951089bc07f29b3909c984c420aed380dd
Tyler Gubala <gubalatyler@yahoo.com>
2020-05-10 12:14:32 -0400

    update for  2.80

--- .vscode/tasks/windows/build_normal.bat
+++ .vscode/tasks/windows/build_normal.bat
@@ -2,6 +2,6 @@ if exist setup.py del setup.py
 copy "bpy\setup.py" .
 del /F /s /q build\*.*
 rmdir /s /q build
-.\venv\3.5-32\Scripts\python setup.py bdist_wheel > logs\3.5-32_normal_build.log 2>&1
-.\venv\3.5-64\Scripts\python setup.py sdist bdist_wheel > logs\3.5-64_normal_build.log 2>&1
+.\venv\3.6-32\Scripts\python setup.py bdist_wheel > logs\3.6-32_normal_build.log 2>&1
+.\venv\3.6-64\Scripts\python setup.py sdist bdist_wheel > logs\3.6-64_normal_build.log 2>&1
 if exist setup.py del setup.py
