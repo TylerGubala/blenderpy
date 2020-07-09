75549f9a470e5c272e269486148cd8a68993096d
Tyler Gubala <gubalatyler@yahoo.com>
2020-07-08 23:19:49 -0400

    install recent wget for building cmake

--- .travis/build_wheels.sh
+++ .travis/build_wheels.sh
@@ -17,7 +17,12 @@ yum install -y gcc gcc-c++ make
 yum install -y git subversion
 yum install -y libX11-devel libXxf86vm-devel libXcursor-devel libXi-devel libXrandr-devel libXinerama-devel
 
-yum install -y wget
+wget http://ftp.gnu.org/gnu/wget/wget-1.16.tar.gz
+yum -y remove wget
+tar -xzvf wget-1.16.tar.gz
+cd wget-1.16
+./configure --with-ssl=openssl --with-libssl-prefix=/usr/lib64/openssl --prefix=/usr
+make && make install
 
 wget https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3.tar.gz
 tar xvzf cmake-3.17.3.tar.gz
