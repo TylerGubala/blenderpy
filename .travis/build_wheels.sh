76265478982b18c1ec48ba478fed3bd505446c5e
Tyler Gubala <gubalatyler@yahoo.com>
2020-07-08 23:28:40 -0400

    try to curl get cmake directly

--- .travis/build_wheels.sh
+++ .travis/build_wheels.sh
@@ -17,13 +17,7 @@ yum install -y gcc gcc-c++ make
 yum install -y git subversion
 yum install -y libX11-devel libXxf86vm-devel libXcursor-devel libXi-devel libXrandr-devel libXinerama-devel
 
-curl -O http://ftp.gnu.org/gnu/wget/wget-1.16.tar.gz
-tar -xzvf wget-1.16.tar.gz
-cd wget-1.16
-./configure --with-ssl=openssl --with-libssl-prefix=/usr/lib64/openssl --prefix=/usr
-make && make install
-
-wget https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3.tar.gz
+curl -O https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3.tar.gz
 tar xvzf cmake-3.17.3.tar.gz
 
 cd cmake-3.17.3
