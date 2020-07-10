8f995b5a4ecec28e82d78fbd041568e2c5d383d6
Tyler Gubala <gubalatyler@yahoo.com>
2020-07-09 21:58:12 -0400

    remove libuv

--- .travis/build_wheels.sh
+++ .travis/build_wheels.sh
@@ -14,16 +14,6 @@ yum install -y gcc gcc-c++ make
 yum install -y git subversion
 yum install -y libX11-devel libXxf86vm-devel libXcursor-devel libXi-devel libXrandr-devel libXinerama-devel libevent-devel
 
-curl -L https://dist.libuv.org/dist/v1.38.1/libuv-v1.38.1.tar.gz -o libuv-v1.38.1.tar.gz
-tar xvzf libuv-v1.38.1.tar.gz
-
-cd libuv-v1.38.1
-sh autogen.sh
-./configure
-make
-make check
-make install
-
 curl -L https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3-Linux-x86_64.tar.gz -o cmake-3.17.3-Linux-x86_64.tar.gz
 tar xvzf cmake-3.17.3-Linux-x86_64.tar.gz
 
