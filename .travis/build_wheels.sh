d9c5170ad2c57f2fb194c2afcb6db48a054007c9
Tyler Gubala <gubalatyler@yahoo.com>
2020-07-17 11:25:53 -0400

    try and update packages

--- .travis/build_wheels.sh
+++ .travis/build_wheels.sh
@@ -13,6 +13,7 @@ function repair_wheel {
 yum install -y gcc gcc-c++ make
 yum install -y git subversion
 yum install -y boost boost-devel fftw-devel freetype freetype-devel giflib glew glew-devel jemalloc libX11-devel libXxf86vm-devel libXcursor-devel libXi-devel libXrandr-devel libXinerama-devel libjpeg-devel libpng-devel libsndfile libtiff libtiff-devel mesa-libGL mesa-libGL-devel OpenEXR OpenEXR-devel SDL SDL_image zlib zlib-devel openssl-devel bzip2-devel libffi-devel yasm
+yum update -y
 
 yum erase -y cmake
 
