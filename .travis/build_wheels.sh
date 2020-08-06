745944fc5655415e1cdd5ae272f4b07be05f21f7
Tyler Gubala <gubalatyler@yahoo.com>
2020-08-06 17:54:49 -0400

    remove glibc installation

--- .travis/build_wheels.sh
+++ .travis/build_wheels.sh
@@ -14,8 +14,6 @@ yum install -y gcc gcc-c++ make
 yum install -y git subversion
 yum install -y boost boost-devel fftw-devel freetype freetype-devel giflib glew glew-devel jemalloc libX11-devel libXxf86vm-devel libXcursor-devel libXi-devel libXrandr-devel libXinerama-devel libjpeg-devel libpng-devel libsndfile libtiff libtiff-devel mesa-libGL mesa-libGL-devel OpenEXR OpenEXR-devel SDL SDL_image zlib zlib-devel openssl-devel bzip2-devel libffi-devel yasm
 
-yum install glibc-2.9
-
 yum erase -y cmake
 
 curl -L https://www.python.org/ftp/python/3.7.7/Python-3.7.7.tgz -o Python-3.7.7.tgz
