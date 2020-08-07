2705645c24cbb72af51cd53f7542e9dc24ac9853
Tyler Gubala <gubalatyler@yahoo.com>
2020-08-06 21:53:35 -0400

    only output errors to the log
    This prevents travisci from stopping the run due to long length

--- .travis/build_wheels.sh
+++ .travis/build_wheels.sh
@@ -19,32 +19,32 @@ yum erase -y cmake
 curl -L https://www.python.org/ftp/python/3.7.7/Python-3.7.7.tgz -o Python-3.7.7.tgz
 tar xzf Python-3.7.7.tgz
 cd Python-3.7.7
-./configure --enable-optimizations
-make install
+./configure --enable-optimizations >> python_install.log
+make install >> python_install.log
 cd ..
 
 curl -L https://www.libraw.org/data/LibRaw-0.19.5.tar.gz -o LibRaw-0.19.5.tar.gz
 tar xzf LibRaw-0.19.5.tar.gz
 cd LibRaw-0.19.5
-./configure
-make
-make install
+./configure >> libraw_install.log
+make >> libraw_install.log
+make install >> libraw_install.log
 cd ..
 
 curl -O -L https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar xjf ffmpeg-snapshot.tar.bz2
 cd ffmpeg
-./configure --disable-x86asm
-make -s
-make install -s
+./configure --disable-x86asm >> ffmpeg_install.log
+make -s >> ffmpeg_install.log
+make install -s >> ffmpeg_install.log
 cd ..
 
 curl -L https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.0/src/hdf5-1.12.0.tar.gz -o hdf5-1.12.0.tar.gz
 tar xzf hdf5-1.12.0.tar.gz
 cd hdf5-1.12.0
-./configure
-make -s
-make install -s
+./configure >> hdf5_install.log
+make -s >> hdf5_install.log
+make install -s >> hdf5_install.log
 cd ..
 
 curl -L https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3-Linux-x86_64.tar.gz -o cmake-3.17.3-Linux-x86_64.tar.gz
@@ -55,36 +55,36 @@ curl -L https://github.com/AcademySoftwareFoundation/openexr/tarball/v2.5.1 | ta
 cd openexr-openexr-*
 mkdir build
 cd build
-cmake ..
-make -s
-make install -s
+cmake .. >> openexr_install.log
+make -s >> openexr_install.log
+make install -s >> openexr_install.log
 cd ../..
 
 git clone https://github.com/AcademySoftwareFoundation/openvdb.git
 cd openvdb
 mkdir build
 cd build
-cmake ..
-make -s
-make install -s
+cmake .. >> openvdb_install.log
+make -s >> openvdb_install.log
+make install -s >> openvdb_install.log
 cd ../..
 
 git clone https://github.com/uclouvain/openjpeg.git
 cd openjpeg
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -s
-make install -s
+cmake .. -DCMAKE_BUILD_TYPE=Release >> openjpeg_install.log
+make -s >> openjpeg_install.log
+make install -s >> openjpeg_install.log
 cd ../..
 
 git clone -b release https://github.com/OpenImageIO/oiio.git
 cd oiio
 mkdir build
 cd build
-cmake ..
-make -s
-make install -s
+cmake .. >> openimageio_install.log
+make -s >> openimageio_install.log
+make install -s >> openimageio_install.log
 cd ../..
 
 # Compile wheels
