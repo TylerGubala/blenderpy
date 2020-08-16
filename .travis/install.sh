f916cd09044481af25353288a391e1e15a55aa65
Tyler Gubala <gubalatyler@yahoo.com>
2020-08-16 14:55:20 -0400

    Add dockerfile

--- .travis/install.sh
+++ .travis/install.sh
@@ -10,10 +10,6 @@ function repair_wheel {
     fi
 }
 
-
-# Install a system package required by our library
-yum install -y atlas-devel
-
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r /io/requirements.txt
