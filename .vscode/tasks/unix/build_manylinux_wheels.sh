85040704f240cf5b6900ec32fab033abd4f8d849
Tyler Gubala <gubalatyler@yahoo.com>
2020-10-11 17:13:59 -0400

    Init freebsd and solaris, fix LONG docker build

--- .vscode/tasks/unix/build_manylinux_wheels.sh
+++ .vscode/tasks/unix/build_manylinux_wheels.sh
@@ -6,7 +6,7 @@ function repair_wheel {
     if ! auditwheel show "$wheel"; then
         echo "Skipping non-platform wheel $wheel"
     else
-        auditwheel repair "$wheel" --plat "$PLAT" -w /blenderpy/wheelhouse/
+        auditwheel repair "$wheel" --plat manylinux2014_x86_64 -w /blenderpy/wheelhouse/
     fi
 }
 
@@ -20,4 +20,9 @@ done
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
     repair_wheel "$whl"
+    rm "$whl"
+done
+
+for whl in wheelhouse/*.whl; do
+    cp "$whl" /blenderpy/dist/
 done
