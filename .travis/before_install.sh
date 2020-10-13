f3410200626a3592632f5f43c743640099c4dca9
Tyler Gubala <gubalatyler@yahoo.com>
2020-10-13 18:43:34 -0400

    test macos travisci build

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,6 +3,23 @@
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
 
     # Install some custom requirements on macOS
+
+    brew install cmake git svn
+
+    mkdir -p /Blender
+    mkdir -p /build/osx
+
+    cd /Blender
+
+    git clone https://git.blender.org/blender.git
+
+    cd blender
+
+    ./make update
+
+    cd /build/osx
+
+    cmake /Blender/blender -DWITH_PYTHON_INSTALL=OFF -DWITH_PYTHON_MODULE=ON
     
 elif [ $TRAVIS_OS_NAME = 'linux' ]; then
 
--- .travis/install.sh
+++ .travis/install.sh
@@ -3,6 +3,10 @@
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
 
     # Install blenderpy on macOS
+
+    cd /build/osx
+
+    make install
     
 elif [ $TRAVIS_OS_NAME = 'linux' ]; then
 
